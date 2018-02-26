6838b3866a708dcc262f0834590deb942e2900ce
wangyakun <512154051@qq.com>
2018-02-26 16:58:38 +0800

    add test

---
+++ src/test/java/debugger/MycatStartupTest.java
@@ -0,0 +1,23 @@
+/*
+* Copyright (c) 2017 Hinew. All Rights Reserved.
+ * ============================================================================
+ * 版权所有 海牛(上海)电子商务有限公司，并保留所有权利。
+ * ----------------------------------------------------------------------------
+ * ----------------------------------------------------------------------------
+ * 官方网站：http://www.hinew.com.cn
+ * ============================================================================
+*/
+package debugger;
+
+import io.mycat.MycatStartup;
+
+/**
+ * @author wangyakun
+ * @email yakun0622@gmail.com
+ * @date 2018/2/25 16:37
+ */
+public class MycatStartupTest {
+    public static void main(String[] args) {
+        MycatStartup.main(args);
+    }
+}
